f9a012509191bba1209ad92d3262890fb05f2fd6
Zuzanna <zuza.nagadowska@gmail.com>
2021-01-26 13:26:27 +0100

    Added the function writePushPop that translates the Push and Pop commands into an assembly code and outputs it to a separate text file

--- Nand2Tetris/nand2tetris/code/VMTranslator.py
+++ Nand2Tetris/nand2tetris/code/VMTranslator.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 import os
-
-sample='''
-push constant 10
-pop local 0
-push constant 21'''
+inputfile = "samplecode.txt"
+outputfile = "sampleasmcode.txt"
 
 def commandType(line):
     if line.startswith('push'):
@@ -35,23 +32,90 @@ def whatArithmetic(line):
         return "NOT"
 
 def getArguments(line):
-    command, arg1, argg = line.split(' ')
-    arg2 = int(argg)
+    command, arga, argb = line.split(' ')
+    arg2 = int(argb)
+    if arga =="constant":
+        arg1 = "constant"
+    elif arga == "local":
+        arg1 = "LCL"
+    elif arga == "argument":
+        arg1 = "ARG"
+    elif arga == "this":
+        arg1 = "THIS"
+    elif arga == "that":
+        arg1 = "THAT"
+    else:
+        print("invalid arg1")
+
     return arg1, arg2
 
+def writePushPop(command,segment,index,line):
+    if command == "PUSH" and segment == "constant":
+        f = open(outputfile,'a')
+        f.write("//"+line)
+        f.write("@"+str(index)+"\n")
+        f.write("D=A"+"\n")
+        f.write("@SP"+"\n")
+        f.write("AM=M+1"+"\n")
+        f.write("A=A-1"+"\n")
+        f.write("M=D"+"\n")
+        f.close()
+    elif command =="PUSH":
+        f = open(outputfile,'a')
+        f.write("//"+line)
+        f.write("@"+segment+"\n")
+        f.write("D=M"+"\n")
+        f.write("@"+str(index)+"\n")
+        f.write("A=A+D"+"\n")
+        f.write("D=M"+"\n")
+        f.write("@SP"+"\n")
+        f.write("AM=M+1"+"\n")
+        f.write("A=A-1"+"\n")
+        f.write("M=D"+"\n")
+        f.close()
+    elif command =="POP":
+        f = open(outputfile,'a')
+        f.write("//"+line)
+        f.write("@"+segment+"\n")
+        f.write("D=M"+"\n")
+        f.write("@"+str(index)+"\n")
+        f.write("D=D+A"+"\n")
+        f.write("@R13"+"\n")
+        f.write("M=D"+"\n")
+        f.write("@SP"+"\n")
+        f.write("AM=M-1"+"\n")
+        f.write("D=M"+"\n")
+        f.write("@R13"+"\n")
+        f.write("A=M"+"\n")
+        f.write("M=D"+"\n")
+        f.close()
+    else:
+        print('Bad line bro')
+
+#def writeArithmetic(segment,line):
 
 def Main():
     #prompt for the file name/location
-    with open('samplecode.txt') as f:
+    with open(inputfile) as f:
         lines = f.readlines()
         for line in lines:
-            line.strip('\n')
-            type = commandType(line)
-            if type == "C_ARITHMETIC":
-                arth_type = whatArithmetic(line)
-            if type == "C_PUSH" or type == "C_POP":
-                arg1, arg2 = getArguments(line)
-            
+            if line.startswith('//'):
+                continue
+            else:
+                line.strip('\n')
+                type = commandType(line)
+                if type == "C_ARITHMETIC":
+                    arg1 = whatArithmetic(line)
+                    newlines = writeArithmetic(arg1,line)
+                elif type == "C_PUSH":
+                    arg1, arg2 = getArguments(line)
+                    newlines= writePushPop("PUSH",arg1,arg2,line)
+                elif type == "C_POP":
+                    arg1, arg2 = getArguments(line)
+                    newlines = writePushPop("POP",arg1,arg2,line)
+                else:
+                    print("invalid type")
+
 
 
 
